fa5a830d12e2fee9c7cc7f90a1ef6adbf6deffe4
Gihun Ham <ham@e-mirim.hs.kr>
2020-09-27 16:22:14 +0900

    메모 액티비티 생성 및 디자인 작업

--- app/src/main/java/kr/hs/emirim/ham/firebasestart/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/ham/firebasestart/MainActivity.java
@@ -7,32 +7,41 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-import com.google.firebase.analytics.FirebaseAnalytics;
-import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import kr.hs.emirim.ham.firebasestart.realtimedb.MemoActivity;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     final static String TAG = "파베-Main";
+    private Button firebaseauthbtn = null;
+    private Button firebaserealtimedbbtn = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "MainActivity onCreate 실행됨");
-        Button firebaseauthbtn = findViewById(R.id.firebaseauthbtn);
+        firebaseauthbtn = findViewById(R.id.firebaseauthbtn);
+        firebaserealtimedbbtn =findViewById(R.id.firebaserealtimdbbtn);
         firebaseauthbtn.setOnClickListener(this);
+        firebaserealtimedbbtn.setOnClickListener(this);
         Log.d(TAG, "MainActivity 버튼 객체 참조 연결하고 리스너 등록");
     }
 
     @Override
     public void onClick(View view) {
         // Toast.makeText(this, "버튼 눌렸어요", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "파베어스 버튼 눌림!");
-
+        Log.d(TAG, "파이어베이스 버튼 눌림!");
+        Intent intent = null;
         switch (view.getId()){
             case R.id.firebaseauthbtn:
                 Log.d(TAG, "파이어베이스 인증 버튼 눌림! ");
-                Intent intent = new Intent(this, AuthActivity.class);
+                intent = new Intent(this, AuthActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.firebaserealtimdbbtn:
+                Log.d(TAG, "파이어베이스 리얼타임디비 버튼 눌림! ");
+                intent = new Intent(this, MemoActivity.class);
                 startActivity(intent);
                 break;
             default:
---
+++ app/src/main/java/kr/hs/emirim/ham/firebasestart/realtimedb/MemoActivity.java
@@ -0,0 +1,16 @@
+package kr.hs.emirim.ham.firebasestart.realtimedb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kr.hs.emirim.ham.firebasestart.R;
+
+public class MemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memo);
+    }
+}
