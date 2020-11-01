038eb599c0e1599a157cc327ca5de31066862666
Gihun Ham <ham@e-mirim.hs.kr>
2020-11-01 15:44:46 +0900

    파이어베이스 스토리지 예제용 액티비티 생성

---
+++ app/src/main/java/kr/hs/emirim/ham/firebasestart/storage/DownloadActivity.java
@@ -0,0 +1,16 @@
+package kr.hs.emirim.ham.firebasestart.storage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kr.hs.emirim.ham.firebasestart.R;
+
+public class DownloadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_download);
+    }
+}
