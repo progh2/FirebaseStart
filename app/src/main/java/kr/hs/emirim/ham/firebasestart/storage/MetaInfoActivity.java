df3e54e24503e05af681af4cfe191a09fc55762d
Gihun Ham <ham@e-mirim.hs.kr>
2020-11-08 16:15:10 +0900

    파이어베이스 스토리지 - 메타인포

---
+++ app/src/main/java/kr/hs/emirim/ham/firebasestart/storage/MetaInfoActivity.java
@@ -0,0 +1,16 @@
+package kr.hs.emirim.ham.firebasestart.storage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kr.hs.emirim.ham.firebasestart.R;
+
+public class MetaInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meta_info);
+    }
+}
