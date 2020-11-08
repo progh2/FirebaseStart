a1aa7f6e413453956b0186ba57f317cd84d4791a
Gihun Ham <ham@e-mirim.hs.kr>
2020-11-08 16:14:21 +0900

    파이어베이스 스토리지 - 파일 업로드 & 다운로드

--- app/src/main/java/kr/hs/emirim/ham/firebasestart/storage/DownloadActivity.java
+++ app/src/main/java/kr/hs/emirim/ham/firebasestart/storage/DownloadActivity.java
@@ -1,16 +1,105 @@
 package kr.hs.emirim.ham.firebasestart.storage;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOError;
+import java.io.IOException;
 
 import kr.hs.emirim.ham.firebasestart.R;
 
-public class DownloadActivity extends AppCompatActivity {
+public class DownloadActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "다운로드액티비티";
+    private File localfile = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_download);
+
+        findViewById(R.id.localimgdownloadbtn).setOnClickListener(this);
+        findViewById(R.id.imgfirebaseuidnbtn).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.localimgdownloadbtn:
+                showDownloadLocalFileImageView();
+                break;
+            case R.id.imgfirebaseuidnbtn:
+                showFirebaseUiDownloadImageView();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void showFirebaseUiDownloadImageView() {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+        StorageReference pathReference = storageRef.child("storage/cat.jpg");
+        ImageView imageView = findViewById(R.id.storageimg);
+        imageView.setAdjustViewBounds(true);
+        Glide.with(this)
+                .using(new FirebaseImageLoader())
+                .load(pathReference)
+                .centerCrop()
+                .override(400,400)
+                .placeholder(R.drawable.alice)
+                .into(imageView);
+    }
+
+    private void showDownloadLocalFileImageView() {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+        StorageReference pathReference = storageRef.child("storage/cat.jpg");
+
+        try{
+            localfile = File.createTempFile("images","jpg");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        pathReference.getFile(localfile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+               long filesize = taskSnapshot.getTotalByteCount();
+               Log.d(TAG,"File Size = " + filesize);
+               Log.d(TAG, "File Name = " + localfile.getAbsolutePath());
+
+                ImageView imageView = (ImageView) findViewById(R.id.storageimg);
+                imageView.setAdjustViewBounds(true);
+                Glide.with(DownloadActivity.this)
+                        .load(new File(localfile.getAbsolutePath()))
+                        .fitCenter()
+                        //.override(800,400)
+                        .placeholder(R.drawable.alice)
+                        .into(imageView);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e(TAG, "onFailure in");
+                e.printStackTrace();
+            }
+        });
+
     }
 }
--- app/src/main/java/kr/hs/emirim/ham/firebasestart/storage/UploadActivity.java
+++ app/src/main/java/kr/hs/emirim/ham/firebasestart/storage/UploadActivity.java
@@ -1,33 +1,62 @@
 package kr.hs.emirim.ham.firebasestart.storage;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnPausedListener;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
 
 import kr.hs.emirim.ham.firebasestart.R;
 
 public class UploadActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String TAG = "업로드액티비티";
     private final int REQ_CODE_SELECT_IMAGE = 1000;
     private String mImgPath = null;
     private String mImgTitle = null;
     private String mImgOrient = null;
+
+    private Button mButton = null;
+    private ProgressBar mProgressBar = null;
+    private ProgressBar mProgressBar2 = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
 
-        findViewById(R.id.uploadbtn).setOnClickListener(this);
+        mButton = findViewById(R.id.uploadbtn);
+        mButton.setOnClickListener(this);
+        mButton.setEnabled(true);
+        mProgressBar = findViewById(R.id.progressBar);
+        mProgressBar.setVisibility(View.GONE);
+        mProgressBar2 = findViewById(R.id.progressBar2);
+        mProgressBar2.setVisibility(View.GONE);
 
         getGallery();
     }
@@ -68,12 +97,25 @@ public class UploadActivity extends AppCompatActivity implements View.OnClickLis
           MediaStore.Images.Media.TITLE,
           MediaStore.Images.Media.ORIENTATION,
         };
+        Cursor cursor = this.getContentResolver().query(uri, proj, null, null, null);
+        cursor.moveToFirst();
+
+        int column_data = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        int column_title = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.TITLE);
+        int column_orientation = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.ORIENTATION);
+
+        mImgPath = cursor.getString(column_data);
+        mImgTitle = cursor.getString(column_title);
+        mImgOrient = cursor.getString(column_orientation);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.uploadbtn:
+                mProgressBar.setVisibility(View.VISIBLE);
+                mProgressBar2.setVisibility(View.VISIBLE);
+                mButton.setEnabled(false);
                 uploadFile(mImgPath);
                 break;
             default:
@@ -81,6 +123,55 @@ public class UploadActivity extends AppCompatActivity implements View.OnClickLis
         }
     }
 
-    private void uploadFile(String mImgPath) {
+    private void uploadFile(String aFilePath) {
+        Uri file = Uri.fromFile(new File(aFilePath));
+        StorageMetadata metadata = new StorageMetadata.Builder()
+                .setContentType("image/jpeg").build();
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+        UploadTask uploadTask = storageRef.child("storage/"
+                + file.getLastPathSegment()).putFile(file,metadata);
+        uploadTask.addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                double progress = (100.0 * snapshot.getBytesTransferred()) / snapshot.getTotalByteCount();
+                mProgressBar2.setProgress((int)progress);
+                Toast.makeText(UploadActivity.this,
+                        "Upload is :" + progress + "% done", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnPausedListener(new OnPausedListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onPaused(@NonNull UploadTask.TaskSnapshot snapshot) {
+                Log.d(TAG, "Upload is Paused");
+                mProgressBar.setVisibility(View.GONE);
+                mProgressBar2.setVisibility(View.GONE);
+                mButton.setEnabled(true);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, "Upload Exception!!");
+                mProgressBar.setVisibility(View.GONE);
+                mProgressBar2.setVisibility(View.GONE);
+                mButton.setEnabled(true);
+            }
+        }).addOnCanceledListener(new OnCanceledListener() {
+            @Override
+            public void onCanceled() {
+                Log.d(TAG, "Upload is Canceled!!");
+                mProgressBar.setVisibility(View.GONE);
+                mProgressBar2.setVisibility(View.GONE);
+                mButton.setEnabled(true);
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Log.d(TAG, "업로드 성공!");
+                mProgressBar.setVisibility(View.GONE);
+                mProgressBar2.setVisibility(View.GONE);
+                mButton.setEnabled(true);
+            }
+        });
     }
 }
