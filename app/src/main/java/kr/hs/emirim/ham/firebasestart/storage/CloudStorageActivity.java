7c9361dc81b7ec3309e491ec31927832b7c4eba3
Gihun Ham <ham@e-mirim.hs.kr>
2020-11-08 13:18:49 +0900

    파이어베이스 스토리지 예제 코드 작업

--- app/src/main/java/kr/hs/emirim/ham/firebasestart/storage/CloudStorageActivity.java
+++ app/src/main/java/kr/hs/emirim/ham/firebasestart/storage/CloudStorageActivity.java
@@ -1,16 +1,80 @@
 package kr.hs.emirim.ham.firebasestart.storage;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import kr.hs.emirim.ham.firebasestart.R;
 
-public class CloudStorageActivity extends AppCompatActivity {
+public class CloudStorageActivity extends AppCompatActivity implements View.OnClickListener {
+    private final int REQUEST_CODE = 777;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cloud_storage);
+
+        findViewById(R.id.uploadbtn).setOnClickListener(this);
+        findViewById(R.id.downloadbtn).setOnClickListener(this);
+        findViewById(R.id.metainfobtn).setOnClickListener(this);
+        findViewById(R.id.deletebtn).setOnClickListener(this);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(new String[]{
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                }, REQUEST_CODE);
+                Toast.makeText(this,
+                        "안드로이드 6부터 일부 권한에 대한 사용자 동의가 필요합니다!",
+                        Toast.LENGTH_LONG).show();
+                findViewById(R.id.uploadbtn).setEnabled(false);
+                findViewById(R.id.downloadbtn).setEnabled(false);
+                findViewById(R.id.metainfobtn).setEnabled(false);
+                findViewById(R.id.deletebtn).setEnabled(false);
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+        switch(v.getId()){
+            case R.id.uploadbtn:
+                intent = new Intent(this, UploadActivity.class);
+                break;
+            case R.id.downloadbtn:
+                intent = new Intent(this, DownloadActivity.class);
+                break;
+            default:
+                break;
+        }
+        if(intent != null){
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_CODE:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    findViewById(R.id.uploadbtn).setEnabled(true);
+                    findViewById(R.id.downloadbtn).setEnabled(true);
+                    findViewById(R.id.metainfobtn).setEnabled(true);
+                    findViewById(R.id.deletebtn).setEnabled(true);
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
--- app/src/main/java/kr/hs/emirim/ham/firebasestart/storage/UploadActivity.java
+++ app/src/main/java/kr/hs/emirim/ham/firebasestart/storage/UploadActivity.java
@@ -1,16 +1,86 @@
 package kr.hs.emirim.ham.firebasestart.storage;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
 
 import kr.hs.emirim.ham.firebasestart.R;
 
-public class UploadActivity extends AppCompatActivity {
+public class UploadActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final int REQ_CODE_SELECT_IMAGE = 1000;
+    private String mImgPath = null;
+    private String mImgTitle = null;
+    private String mImgOrient = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
+
+        findViewById(R.id.uploadbtn).setOnClickListener(this);
+
+        getGallery();
+    }
+
+    private void getGallery() {
+        Intent intent = null;
+        if(Build.VERSION.SDK_INT >= 19){
+            intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        }else{
+            intent = new Intent(Intent.ACTION_GET_CONTENT);
+        }
+        intent.setType("image/*");
+        startActivityForResult(intent,REQ_CODE_SELECT_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQ_CODE_SELECT_IMAGE){
+            if(resultCode == Activity.RESULT_OK){
+                Uri uri = data.getData();
+                getImageNameToUri(uri);
+
+                try{
+                    Bitmap bm = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+                    ImageView img = findViewById(R.id.showimg);
+                    img.setImageBitmap(bm);
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void getImageNameToUri(Uri uri) {
+        String[] proj = {
+          MediaStore.Images.Media.DATA,
+          MediaStore.Images.Media.TITLE,
+          MediaStore.Images.Media.ORIENTATION,
+        };
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.uploadbtn:
+                uploadFile(mImgPath);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void uploadFile(String mImgPath) {
     }
 }
